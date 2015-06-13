c482da21d77e3b48a497767c0549f7a598ad8e17
Ken Reese <kreese@instructure.com>
2015-06-12 22:14:10 -0600

    Added while loop example.

--- 05ForLoops/main.go
+++ 05ForLoops/main.go
@@ -11,7 +11,21 @@ func main() {
 
   fmt.Println("End of loop, look carefully for the next one!")
 
+  // Notice no parens
   for i := 0; i < 10; i++ {
     fmt.Printf("Counting %d\n", i);
+  }
+
+  // No such thing as while loops, just modify the for loop:
+  keepGoing := true
+  for keepGoing {
+    fmt.Print("Do you want to keep looping? (y/n) ")
+    var _going string
+    fmt.Scanf("%s", &_going)
+    if _going == "n" {
+      keepGoing = false
+    } else if _going != "y" {
+      fmt.Printf("%s is not a valid option...", _going)
+    }
   }
 }
